5bb1a90b2dbaf2da50f82297ae0aa0a6840de668
Harsh Patwari <hpat1996@cs.washington.edu>
2019-05-07 12:56:33 -0700

    Added experiments. Fixed bug related for bad init of data

--- stackedAutoencoders.py
+++ stackedAutoencoders.py
@@ -1,6 +1,12 @@
 import sys
 import numpy as np
 
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+plt.ioff()
+
 import torch
 from torch.autograd import Variable
 
@@ -10,6 +16,8 @@ import torch.nn.parallel
 import torch.optim as optim
 import torch.utils.data
 
+####################################################################################################
+# CONSTANTS AND HYPERPARAMETERS
 ####################################################################################################
 
 # Use GPU if available
@@ -34,11 +42,13 @@ NUM_TRAIN                   = (0.7, 0.7)
 LEARNING_RATE               = 0.01
 WEIGHT_DECAY                = 0.0
 LOSS_FUNCTION               = 'MSE'
-NUM_ITERATIONS              = 200
+NUM_ITERATIONS              = 100
 
 print("\n")
 print("Initializing...")
 
+####################################################################################################
+# DATASET
 ####################################################################################################
 
 # Normalize the ratings in the data set.
@@ -72,7 +82,9 @@ num_train_users   = int(NUM_TRAIN[0] * num_users)
 num_train_jokes   = int(NUM_TRAIN[1] * num_jokes)
 
 train_data  = np.zeros(shape=data.shape)
+train_data.fill(NORMALIZED_UNKNOWN_RATING)
 test_data   = np.zeros(shape=data.shape)
+test_data.fill(NORMALIZED_UNKNOWN_RATING)
 
 train_data  [               :               ,                       :num_train_jokes]   = data[               :               ,                       :num_train_jokes] 
 train_data  [               :num_train_users,                       :               ]   = data[               :num_train_users,                       :               ] 
@@ -82,6 +94,8 @@ test_data   [num_train_users:               ,   num_train_jokes     :           
 train_data  = torch.tensor(train_data,  device = DEVICE, dtype=torch.float)
 test_data   = torch.tensor(test_data,   device = DEVICE, dtype=torch.float)
 
+####################################################################################################
+# STACKED AUTOENCODER MODEL
 ####################################################################################################
 
 # The stacked auto encoder model
@@ -130,6 +144,8 @@ class StackedAutoEncoder(nn.Module):
         return x
 
 ####################################################################################################
+# LOSS FUNCTIONS
+####################################################################################################
 
 # MSE Loss function
 def MSE_Loss(predicted, actual):
@@ -161,55 +177,101 @@ def getLoss(predicted, actual, loss_function='MSE'):
         return RMSE_Loss(predicted, actual)
 
 ####################################################################################################
+# TRAIN AND TEST
+####################################################################################################
 
-mode = sys.argv[1]
-
-if (mode == 'train'):
-
-    # Training on train data
+def train(learing_rate, weight_decay, loss_function, num_iterations, save_model=False):
+        # Training on train data
     stackedAutoEncoder  = StackedAutoEncoder().to(DEVICE)
-    optimizer           = optim.Adam(stackedAutoEncoder.parameters(), lr = LEARNING_RATE, weight_decay = WEIGHT_DECAY)
+    optimizer           = optim.Adam(stackedAutoEncoder.parameters(), lr = learing_rate, weight_decay = weight_decay)
 
     print("Training...")
     # Train the model
-    for i in range(NUM_ITERATIONS):
+    epoch_loss = []
+    for i in range(num_iterations):
         predicted_ratings = stackedAutoEncoder(train_data)
 
         optimizer.zero_grad() 
-        loss = getLoss(predicted_ratings, train_data, LOSS_FUNCTION)
+        loss = getLoss(predicted_ratings, train_data, loss_function)
         loss.backward()
         optimizer.step()
 
+        epoch_loss.append((i + 1, loss.data.item()))
         print("Epoch #", (i + 1), ": Training loss: ", loss.data.item())
 
     print("Training finished.\n")
 
+    test_loss = test(stackedAutoEncoder, loss_function)
+
+    if (save_model):
+        print("Saving model...")
+        torch.save(stackedAutoEncoder, "model")
+        print("Saved model.")
+    
+    return epoch_loss, test_loss
+
+
+def test(model, loss_function):
     print("Testing...")
-    predicted_ratings = stackedAutoEncoder(test_data)
-    loss = getLoss(predicted_ratings, test_data, LOSS_FUNCTION)
-    print("Loss on test data: ", loss.data.item())
+    predicted_ratings = model(test_data)
+    test_loss = getLoss(predicted_ratings, test_data, loss_function).data.item()
+    print("Loss on test data: ", test_loss)
     print("\n")
 
-    print("Saving model...")
-    torch.save(stackedAutoEncoder, "model")
-    print("Saved model.")
-    
+    return test_loss
+
+####################################################################################################
+# EXPERIMENTATION
+####################################################################################################
+
+def plot_images(plot_data, labels, xlabel, ylabel, filename):
+    plt.clf()
+    for data, label in zip(plot_data, labels):
+        xs = [x[0] for x in data]
+        ys = [y[1] for y in data]
+        plt.plot(xs, ys, label=label)
+    plt.legend(loc='best')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.savefig(filename)
+    plt.clf()
+
+def experiment_learning_rate():
+    learning_rates = [i / 100.0 for i in range(1, 10)]
+    plot_data = []
+    labels = []
+    for learning_rate in learning_rates:
+        epoch_loss, _ = train(learning_rate, WEIGHT_DECAY, "MSE", NUM_ITERATIONS, True)
+        plot_data.append(epoch_loss[10:])
+        labels.append("Learning rate: " + str(learning_rate))
+    plot_images(plot_data, labels, "Epoch", "Mean squared error", "VaryingLearningRate.png")
+
+def run_experiments():
+    experiment_learning_rate()
+
+####################################################################################################
+# USER INTERACTION FOR TRAINING AND TESTING MODELS
+####################################################################################################
+
+mode = sys.argv[1]
+
+if (mode == 'train'):
+    # Training on train data
+    train(LEARNING_RATE, WEIGHT_DECAY, LOSS_FUNCTION, NUM_ITERATIONS, True)
 
 elif (mode == 'test'):
-
     # Testing on test data
     print("Loading model...")
     stackedAutoEncoder = torch.load("model")
     print("Loaded model.")
-    print("Testing...")
 
-    predicted_ratings = stackedAutoEncoder(test_data)
-    loss = getLoss(predicted_ratings, test_data, LOSS_FUNCTION)
-    
-    print("Loss on test data: ", loss.data.item())
+    test(stackedAutoEncoder, LOSS_FUNCTION)
 
+elif (mode == 'exp'):
+    run_experiments()
 
 else:
-    print("Usage: python3 stackedAutoencoders.py <train | test>")
+    print("Usage: python3 stackedAutoencoders.py <train | test | exp>")
 
 print('\n')
+
