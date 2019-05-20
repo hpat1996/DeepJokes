ca35a5f8cc70106acd94d73fa24e1fff8ac67158
Harsh Patwari <hpat1996@cs.washington.edu>
2019-05-19 17:29:17 -0700

    Moved images to folder. Added dev data

--- stackedAutoencoders.py
+++ stackedAutoencoders.py
@@ -35,8 +35,10 @@ NORMALIZED_MIN_RATING       = 1
 NORMALIZED_MAX_RATING       = 5
 NORMALIZED_ROUNDED          = True
 
-                            # (% users, % jokes)
-NUM_TRAIN                   = (0.7, 0.7)
+NUM_DEV_TEST_USERS          = 0.5
+NUM_DEV_JOKES               = 0.3
+NUM_TEST_JOKES              = 0.2
+NUM_DEV_TEST_JOKES          = NUM_DEV_JOKES + NUM_TEST_JOKES
 
 # Hyperparameters for the model
 LEARNING_RATE               = 0.04
@@ -61,7 +63,7 @@ def normalizeData(n):
     dataset_range       = (DATASET_MAX_RATING    -  DATASET_MIN_RATING)
     normalized_range    = (NORMALIZED_MAX_RATING -  NORMALIZED_MIN_RATING)
     
-    normalized_n = (((n - DATASET_MIN_RATING) * normalized_range) / dataset_range) + NORMALIZED_MIN_RATING
+    normalized_n        = (((n - DATASET_MIN_RATING) * normalized_range) / dataset_range) + NORMALIZED_MIN_RATING
 
     if NORMALIZED_ROUNDED:
         normalized_n = round(normalized_n)
@@ -77,22 +79,27 @@ data = np.vectorize(normalizeData)(data)
 np.random.shuffle(data)
 num_users, num_jokes = data.shape
 
+# Divide the data into train, dev and test
+train_data  = np.copy(data)
 
-# Divide the data into train and test
-num_train_users   = int(NUM_TRAIN[0] * num_users)
-num_train_jokes   = int(NUM_TRAIN[1] * num_jokes)
+dev_data    = np.zeros(shape=data.shape)
+dev_data.fill(NORMALIZED_UNKNOWN_RATING)
 
-train_data  = np.zeros(shape=data.shape)
-train_data.fill(NORMALIZED_UNKNOWN_RATING)
 test_data   = np.zeros(shape=data.shape)
 test_data.fill(NORMALIZED_UNKNOWN_RATING)
 
-train_data  [               :               ,                       :num_train_jokes]   = data[               :               ,                       :num_train_jokes] 
-train_data  [               :num_train_users,                       :               ]   = data[               :num_train_users,                       :               ] 
-test_data   [num_train_users:               ,   num_train_jokes     :               ]   = data[num_train_users:               ,   num_train_jokes     :               ]
+num_dev_test_users  = int(NUM_DEV_TEST_USERS    * num_users)
+num_dev_jokes       = int(NUM_DEV_JOKES         * num_jokes)
+num_test_jokes      = int(NUM_TEST_JOKES        * num_jokes)
+num_dev_test_jokes  = int(NUM_DEV_TEST_JOKES    * num_jokes)
+
+train_data  [num_dev_test_users : , num_dev_test_jokes  :               ]   = NORMALIZED_UNKNOWN_RATING
+dev_data    [num_dev_test_users : , -num_dev_test_jokes : -num_dev_jokes]   = data[num_dev_test_users : , -num_dev_test_jokes   : -num_dev_jokes]
+test_data   [num_dev_test_users : , -num_dev_jokes      :               ]   = data[num_dev_test_users : , -num_dev_jokes        :               ]
 
 
 train_data  = torch.tensor(train_data,  device = DEVICE, dtype=torch.float)
+dev_data    = torch.tensor(dev_data,    device = DEVICE, dtype=torch.float)
 test_data   = torch.tensor(test_data,   device = DEVICE, dtype=torch.float)
 
 ####################################################################################################
@@ -202,15 +209,23 @@ def train(learing_rate, weight_decay, loss_function, num_iterations, save_model=
 
     print("Training finished.\n")
 
-    test_loss = test(stackedAutoEncoder, loss_function)
+    dev_loss = dev(stackedAutoEncoder, loss_function)
 
     if (save_model):
         print("Saving model...")
         torch.save(stackedAutoEncoder, "model")
         print("Saved model.")
     
-    return epoch_loss, test_loss
+    return epoch_loss, dev_loss
 
+def dev(model, loss_function):
+    print("Evaluating...")
+    predicted_ratings = model(dev_data)
+    dev_loss = getLoss(predicted_ratings, dev_data, loss_function).data.item()
+    print("Loss on dev data: ", dev_loss)
+    print("\n")
+
+    return dev_loss
 
 def test(model, loss_function):
     print("Testing...")
